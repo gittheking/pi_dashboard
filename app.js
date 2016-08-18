abac2176139f7cd4ed6ec74db7e4b7e8ae1505d1
Bobby King <king.robertjoseph@gmail.com>
2016-08-18 19:13:34 -0400

    Add index.html with home.js routes controller

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger  = require('morgan');
 const path    = require('path');
+const home    = require('./routes/home');
 
 const app     = express();
 const port    = process.env.PORT || 3000;
@@ -9,3 +10,5 @@ app.use(express.static(path.join(__dirname,'public')));
 app.use(logger('dev'));
 
 app.listen(port, () => console.log('Server is listening on port: ',port));
+
+app.use('/',home);
