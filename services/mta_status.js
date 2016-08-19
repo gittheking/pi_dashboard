6a00340ee9151bea07d9efe38ea555f511fb94c7
Bobby King <king.robertjoseph@gmail.com>
2016-08-19 18:56:50 -0400

    Refactor getTrainStatus

--- services/mta_status.js
+++ services/mta_status.js
@@ -10,17 +10,7 @@ function getTrainStatus(req,res,next) {
     response.on('end', () => {
       parseString(completeResponse, (err, result) => {
         if(err) throw err;
-        let allTrains = result.service.subway[0].line;
-        // let myTrains  = allTrains.filter( train => {
-        //   for(let i = 0; i < trains.length; i++) {
-        //     if(train.name[0].indexOf(trains[i]) >= 0) return true;
-        //   }
-        //   return false;
-        // });
-        allTrains.forEach( train => {
-          if(train.text[0] !== '') train.text[0] = formatStatusText(train.text[0]);
-        })
-        res.trains = allTrains;
+        res.trains = result.service.subway[0].line;
         next(); 
       });
     });
