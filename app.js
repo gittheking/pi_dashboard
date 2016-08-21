267aad96d9b77003e14f8e60ac395c649351b4cf
Bobby King <king.robertjoseph@gmail.com>
2016-08-20 21:46:05 -0400

    Set up architecture for Sonos service

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const logger  = require('morgan');
 const path    = require('path');
+
 const home    = require('./routes/home');
 const train   = require('./routes/train');
+const music   = require('./routes/music');
 
 const app     = express();
 const port    = process.env.PORT || 3000;
@@ -14,3 +16,4 @@ app.listen(port, () => console.log('Server is listening on port: ',port));
 
 app.use('/',home);
 app.use('/train',train);
+app.use('/music',music);
