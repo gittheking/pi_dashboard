fc4a907377f60708fae4b7e222aa15c97a99c7a3
Bobby King <king.robertjoseph@gmail.com>
2016-08-22 19:10:54 -0400

    Update and refactor main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,19 @@
 'use strict';
 (() => {
-  const trackArtist = document.querySelector('#artist');
-  const trackTitle  = document.querySelector('#track');
-  const trackAlbum  = document.querySelector('#album');
+  const trackAlbumArt = document.querySelector('#album-art');
+  const trackArtist   = document.querySelector('#artist');
+  const trackTitle    = document.querySelector('#track');
+  const trackAlbum    = document.querySelector('#album');
   window.setInterval(() => {
     fetch('/music')
     .then(response => response.json())
     .then( result => {
-      if(result.track) {
-        trackArtist.innerText = result.track.artist;
-        trackTitle.innerText  = result.track.title;
-        trackAlbum.innerText  = result.track.album;
-      }
+      if(result.trackInfo && result.trackInfo.title !== trackTitle.innerText) {
+        trackAlbumArt.src     = result.trackInfo.albumArtURL;
+        trackArtist.innerText = result.trackInfo.artist;
+        trackTitle.innerText  = result.trackInfo.title;
+        trackAlbum.innerText  = result.trackInfo.album;
+      } 
     });
   }, 3000)
 })();
