befa27c9d56b998e9b18342ce25e884f382bfde8
Bobby King <king.robertjoseph@gmail.com>
2016-08-25 18:32:50 -0400

    Refactor mta_status to use node-fetch

--- services/mta_status.js
+++ services/mta_status.js
@@ -1,20 +1,16 @@
-const http        = require('http');
+const fetch       = require('node-fetch');
 const parseString = require('xml2js').parseString;
 
 function getTrainStatus(req,res,next) {
-  http.get('http://web.mta.info/status/serviceStatus.txt', function(response) {
-    let completeResponse = '';
-    response.on('data', chunk => {
-      completeResponse += chunk;
-    });
-    response.on('end', () => {
-      parseString(completeResponse, (err, result) => {
-        if(err) throw err;
-        res.trains = result.service.subway[0].line;
-        next(); 
-      });
-    });
-  });
+  fetch('http://web.mta.info/status/serviceStatus.txt')
+  .then( response => response.text())
+  .then( data => {
+    parseString(data,(err,result) => {
+      if(err) throw err;
+      res.trains = result.service.subway[0].line;
+      next();
+    })
+  })
 }
 
 function formatStatusText(statusText) {
