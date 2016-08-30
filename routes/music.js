98454b62a30157a8518cd640381b7a79e65dff69
Bobby King <king.robertjoseph@gmail.com>
2016-08-29 22:24:17 -0400

    Add routes for sonos control and refactored most of music.js

--- routes/music.js
+++ routes/music.js
@@ -1,16 +1,39 @@
-const router                  = require('express').Router();
-const { getCurrentTrack,
-        getCurrentVolume,
-        updateVolume     }    = require('../services/sonos');
+const router = require('express').Router();
+const sonos  = require('../services/sonos');
 
-router.get('/',getCurrentTrack,getCurrentVolume,(req,res) => {
-  res.json({
-            trackInfo: res.trackInfo,
-            volume:    res.volume
-           });
-});
+function sendInfo(req, res) {
+  res.json({trackInfo: res.trackInfo});
+}
 
-router.put('/:volume',updateVolume,(req,res) => {
+function sendState(req,res) {
+  let result;
+  if(res.error) {
+    result = res.error;
+  } else {
+    result = 'success';
+  }
+  res.json({response: result});
+}
+
+router.get('/track', sonos.getCurrentTrack, sendInfo);
+
+router.get('/next', sonos.playNext, sonos.getCurrentTrack, sendInfo);
+
+router.get('/previous', sonos.playPrevious, sonos.getCurrentTrack, sendInfo);
+
+router.get('/play', sonos.play, sonos.getState, sendState);
+
+router.get('/stop', sonos.stop, sonos.getState, sendState);
+
+router.get('/volume', sonos.getCurrentVolume, (req,res) => {
+  res.json({volume: res.volume});
+})
+
+router.get('/state', sonos.getState, (req,res) => {
+  res.json({state: res.state});
+})
+
+router.put('/volume/:value', sonos.updateVolume, (req,res) => {
   console.log(req.params);
 });
 
