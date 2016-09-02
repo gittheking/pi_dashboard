6a05d7ba6d8935c178e6c4422cd248202281ab6f
Bobby King <king.robertjoseph@gmail.com>
2016-09-02 18:47:43 -0400

    Add MusicControl as a React stateful React component

--- src/client/app/components/MusicControls.jsx
+++ src/client/app/components/MusicControls.jsx
@@ -1,20 +1,67 @@
 import React from 'react';
 
-const MusicControls = props => {
-  return (
-    <div className="track-controls">
-      <div className="track-control-buttons">
-        <img src="/img/previous.svg" className="previous"/>
-        <img src="/img/play.svg" className="play" />
-        <img src="/img/stop.svg" className="stop" />
-        <img src="/img/next.svg" className="next" />
-      </div>
-      <div className="volume-control">
-        <img src="/img/volume.svg" className="volume-icon" />
-        <input className="volume" type="range" />
-      </div>
-    </div>
-  )
-}
+export default class MusicControls extends React.Component {
+  
+  constructor() {
+    super();
 
-export default MusicControls;
+    this.state = {
+      playState: 'playing',
+      volume: '50'
+    }
+
+    this.getVolume();
+    this.getPlayState();
+  }
+
+  getPlayState() {
+    fetch('/music/state')
+    .then(response => response.json())
+    .then(result => this.setState({playState: result.state}))
+    .catch(err => console.log('Fetch Error: ', err));
+  }
+
+  getVolume() {
+    fetch('/music/volume')
+    .then(response => response.json())
+    .then(result => this.setState({volume: result.volume}))
+    .catch(err => console.log('Fetch error: ',err));
+  }
+
+  setVolume(volume) {
+    console.log('Volume set at: ',volume);
+    // fetch(`/music/volume/${volume}`, {
+    //   method: 'PUT'
+    // })
+    // .catch(err => console.log('Fetch Error: ', err));
+  }
+
+  playState() {
+    if(this.state.playState === 'paused' || this.state.playState === 'paused') {
+      return <img src="/img/play.svg" className="play" />
+    } else {
+      return <img src="/img/stop.svg" className="stop" />
+    }
+  }
+
+  render() {
+    return (
+      <div className="track-controls">
+        <div className="track-control-buttons">
+          <img src="/img/previous.svg" className="previous"/>
+          {this.playState()}
+          <img src="/img/next.svg" className="next" />
+        </div>
+        <div className="volume-control">
+          <img src="/img/volume.svg" className="volume-icon" />
+          <input
+            className="volume"
+            type="range"
+            value={this.state.volume}
+            onChange={event => this.setState({volume: event.target.value})}
+            onMouseUp={event => this.setVolume(event.target.value)} />
+        </div>
+      </div>
+    )
+  }
+}
