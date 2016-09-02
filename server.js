937136d31c8ddd793fafb1ffda792c8ef6e4e2c3
Bobby King <king.robertjoseph@gmail.com>
2016-09-01 20:13:20 -0400

    Change publicly served directory from public to dist

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const music   = require('./routes/music');
 const app     = express();
 const port    = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'dist')));
 app.use(logger('dev'));
 
 app.listen(port, () => console.log('Server is listening on port: ',port));
