1b2b4f5ca7949616d1c401ba1a77c83408ba227b
Bobby King <king.robertjoseph@gmail.com>
2016-09-06 13:05:29 -0400

    Move Sonos app out to it's own component

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -1,53 +1,13 @@
-// import the libs we need
 import React            from 'react';
 import ReactDOM         from 'react-dom';
-import MusicInfo        from './components/MusicInfo.jsx';
-import MusicControls    from './components/MusicControls.jsx';
+import Sonos            from './components/Sonos.jsx';
 
-// create a React Component called _App_
+
 export default class App extends React.Component {
-
-  constructor() {
-    super();
-
-    this.state = {
-      artist: 'Artist',
-      track: 'Track',
-      album: 'Album',
-      albumCoverURL: ''
-    }
-
-    this.getTrackInfo();
-  }
-
-  getTrackInfo() {
-    fetch('/music/track')
-    .then(response => response.json())
-    .then(result => {
-      this.setState({
-        artist: result.trackInfo.artist,
-        track: result.trackInfo.title,
-        album: result.trackInfo.album,
-        albumCoverURL: result.trackInfo.albumArtURL,
-        volume: '50'
-      });
-    })
-    .catch(err => console.log('Fetch error: ',err));
-  }
 
   render() {
     return (
-      <div>
-        <h1>SONOS</h1>
-        <MusicInfo 
-          artist={this.state.artist}
-          track={this.state.track}
-          album={this.state.album}
-          albumCoverURL={this.state.albumCoverURL} />
-        <MusicControls 
-          playState={this.state.playState}
-          volume={this.state.volume} />
-      </div>
+      <Sonos />
     )
   };
 }
