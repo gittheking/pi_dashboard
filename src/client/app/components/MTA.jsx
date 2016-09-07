279f756bf998f8b71009262265901e9ed3426bb9
Bobby King <king.robertjoseph@gmail.com>
2016-09-06 23:52:59 -0400

    Add MTA component

---
+++ src/client/app/components/MTA.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default class MTA extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      
+    )
+  };
+}
