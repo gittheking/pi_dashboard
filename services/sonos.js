88a9fb001e8936bb38f17dd78283f7b7ba439c35
Bobby King <king.robertjoseph@gmail.com>
2016-09-22 15:04:25 -0400

    Add _some_ AirBnB styling to sonos.js

--- services/sonos.js
+++ services/sonos.js
@@ -1,10 +1,10 @@
 const sonos = require('sonos');
 
-function getCurrentTrack(req,res,next) {
-  sonos.search((device,model) => {
-    if( model !== 'BR100') {
-      device.currentTrack((err,info) => {
-        if(err) console.log('Error: ',err);
+function getCurrentTrack(req, res, next) {
+  sonos.search((device, model) => {
+    if (model !== 'BR100') {
+      device.currentTrack((err, info) => {
+        if (err) console.log('Error: ', err);
         res.trackInfo = info;
         next();
       });
@@ -12,78 +12,78 @@ function getCurrentTrack(req,res,next) {
   });
 }
 
-function getState(req,res,next) {
-  sonos.search((device,model) => {
-    if(model !== 'BR100') {
-      device.getCurrentState((err,state) => {
+function getState(req, res, next) {
+  sonos.search((device, model) => {
+    if (model !== 'BR100') {
+      device.getCurrentState((err, state) => {
         res.state = state;
         next();
-      })
+      });
     }
   });
 }
 
-function getCurrentVolume(req,res,next) {
-  sonos.search((device,model) => {
-    if( model !== 'BR100') {
-      device.getVolume((err,volume) => {
-        if(err) console.log('Error: ',err);
+function getCurrentVolume(req, res, next) {
+  sonos.search((device, model) => {
+    if (model !== 'BR100') {
+      device.getVolume((err, volume) => {
+        if (err) console.log('Error: ', err);
         res.volume = volume;
         next();
-      })
+      });
     }
-  })
+  });
 }
 
-function playNext(req,res,next) {
-  sonos.search((device,model) => {
-    if(model !== 'BR100') {
-      device.next((err,nexted) => {
-        if(err) res.error = 'Error: You be at the end of the queue'
+function playNext(req, res, next) {
+  sonos.search((device, model) => {
+    if (model !== 'BR100') {
+      device.next((err) => {
+        if (err) res.error = 'Error: You be at the end of the queue';
         next();
       });
     }
   });
 }
 
-function play(req,res,next) {
-  sonos.search((device,model) => {
-    if(model !== 'BR100') {
-      device.play((err,playing) => {
-        if(err) console.log('Error: ',err);
+function play(req, res, next) {
+  sonos.search((device, model) => {
+    if (model !== 'BR100') {
+      device.play((err) => {
+        if (err) console.log('Error: ', err);
         next();
       });
     }
   });
 }
 
-function stop(req,res,next) {
-  sonos.search((device,model) => {
-    if(model !== 'BR100') {
-      device.pause((err,paused) => {
-        if(err) res.error = 'Error: There was an error pausing/stopping the music';
+function stop(req, res, next) {
+  sonos.search((device, model) => {
+    if (model !== 'BR100') {
+      device.pause((err) => {
+        if (err) res.error = 'Error: There was an error pausing/stopping the music';
         next();
       });
     }
   });
 }
 
-function playPrevious(req,res,next) {
-  sonos.search((device,model) => {
-    if(model !== 'BR100') {
-      device.previous((err,previoused) => {
-        if(err) res.error = 'Error: You may be at the begining of the queue';
+function playPrevious(req, res, next) {
+  sonos.search((device, model) => {
+    if (model !== 'BR100') {
+      device.previous((err) => {
+        if (err) res.error = 'Error: You may be at the begining of the queue';
         next();
       });
     }
   });
 }
 
-function updateVolume(req,res,next) {
-  sonos.search((device,model) => {
-    if( model !== 'BR100') {
-      device.setVolume(req.params.value, err => {
-        if(err) res.error = 'Error: There was an issue with updating the volume';
+function updateVolume(req, res, next) {
+  sonos.search((device, model) => {
+    if (model !== 'BR100') {
+      device.setVolume(req.params.value, (err) => {
+        if (err) res.error = 'Error: There was an issue with updating the volume';
         next();
       });
     }
