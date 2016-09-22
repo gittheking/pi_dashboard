910a8112fc36c8d23f6915e1337bfd8e21a92933
Bobby King <king.robertjoseph@gmail.com>
2016-09-22 14:59:39 -0400

    Add _some_ AirBnB style to train route

--- routes/train.js
+++ routes/train.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const mta    = require('../services/mta_status');
 
-router.get('/',mta.getTrainStatus,(req,res) => {
-  res.json({status: res.trains});
+router.get('/', mta.getTrainStatus, (req, res) => {
+  res.json({ status: res.trains });
 });
 
 module.exports = router;
