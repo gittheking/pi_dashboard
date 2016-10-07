82578239003f007f320ad809841fd3e93addc3c5
Bobby King <king.robertjoseph@gmail.com>
2016-10-07 10:25:04 -0400

    Add stateless functional component MTACurrentLine

--- src/client/app/components/MTA.jsx
+++ src/client/app/components/MTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import MTACurrentLine from './MTACurrentLine.jsx';
 
 export default class MTA extends React.Component {
 
@@ -17,23 +17,23 @@ export default class MTA extends React.Component {
   getTrains() {
     fetch('/train')
     .then(response => response.json())
-    .then(result => {
-      let trains = [];
-      result.status.forEach(train => trains.push(train.name[0]))
-      this.setState({trains: trains});
-    })
+    .then((result) => {
+      const trains = [];
+      result.status.forEach(train => trains.push(train.name[0]));
+      this.setState({ trains });
+    });
   }
 
   displayTrains() {
-    return this.state.trains.map(line => {
+    return this.state.trains.map((line) => {
       return (
         <li key={line}>
           <div className="subway-list-item">
             {line}
           </div>
         </li>
-      )
-    })
+      );
+    });
   }
 
   render() {
@@ -41,12 +41,13 @@ export default class MTA extends React.Component {
       <div>
         <h1>Trains</h1>
         <h2>Current Line</h2>
-        <h2>{this.state.currentLine}</h2>
+        <MTACurrentLine 
+        currentLine={this.state.currentLine} />
         <h3>All Lines</h3>
         <ul className="subway-list">
           {this.displayTrains()}
         </ul>
       </div>
-    )
-  };
+    );
+  }
 }
---
+++ src/client/app/components/MTACurrentLine.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MTACurrentLine = (props) => {
+  return (
+    <div>
+      { props.currentLine }
+    </div>
+  );
+}
+
+export default MTACurrentLine;
