ad268898473622f2f8a984fb719075cf81216c5e
Bobby King <king.robertjoseph@gmail.com>
2016-10-09 19:08:14 -0400

    Add modular styling for MusicControls component

--- src/client/app/components/MusicControls.jsx
+++ src/client/app/components/MusicControls.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './MusicControls_style.js';
 
 export default class MusicControls extends React.Component {
-  
+
   constructor() {
     super();
 
     this.state = {
       playState: 'playing',
-      volume: '50'
-    }
+      volume: '50',
+    };
 
     this.getVolume();
     this.getPlayState();
@@ -17,31 +18,31 @@ export default class MusicControls extends React.Component {
   getPlayState() {
     fetch('/music/state')
     .then(response => response.json())
-    .then(result => this.setState({playState: result.state}))
+    .then(result => this.setState({ playState: result.state }))
     .catch(err => console.log('Fetch Error: ', err));
   }
 
   getVolume() {
     fetch('/music/volume')
     .then(response => response.json())
-    .then(result => this.setState({volume: result.volume}))
+    .then(result => this.setState({ volume: result.volume }))
     .catch(err => console.log('Fetch error: ',err));
   }
 
   setVolume(volume) {
     console.log('Volume set at: ',volume);
     fetch(`/music/volume/${volume}`, {
-      method: 'PUT'
+      method: 'PUT',
     })
-    .then(response => console.log('Response: ',response))
+    .then(response => console.log('Response: ', response))
     .catch(err => console.log('Fetch Error: ', err));
   }
 
   play() {
     fetch('/music/play')
     .then(response => response.json())
-    .then(result => {
-      this.setState({playState: 'playing'});
+    .then((result) => {
+      this.setState({ playState: 'playing' });
     })
     .catch(err => console.log('Fetch Error: ', err));
   }
@@ -49,8 +50,8 @@ export default class MusicControls extends React.Component {
   stop() {
     fetch('/music/stop')
     .then(response => response.json())
-    .then(result => {
-      this.setState({playState: 'stopped'});
+    .then((result) => {
+      this.setState({ playState: 'stopped' });
     })
     .catch(err => console.log('Fetch Error: ', err));
   }
@@ -58,57 +59,71 @@ export default class MusicControls extends React.Component {
   playNext() {
     fetch('/music/next')
     .then(response => response.json())
-    .catch(err => console.log('Fetch error: ',err));
+    .catch(err => console.log('Fetch error: ', err));
   }
 
   playPrevious() {
     fetch('/music/previous')
     .then(response => response.json())
-    .catch(err => console.log('Fetch error: ',err));
+    .catch(err => console.log('Fetch error: ', err));
   }
 
   playState() {
-    if(this.state.playState === 'paused' || this.state.playState === 'stopped') {
+    if (this.state.playState === 'paused' || this.state.playState === 'stopped') {
       return (
-        <img 
+        <img
           src="/img/play.svg"
-          className="play"
-          onClick={this.play.bind(this)} />
-      )
+          alt="Play"
+          style={styles.controlButtons}
+          onClick={this.play.bind(this)}
+        />
+      );
     } else {
       return (
         <img
           src="/img/stop.svg"
-          className="stop"
-          onClick={this.stop.bind(this)} />
-      )
+          alt="Stop"
+          style={styles.controlButtons}
+          onClick={this.stop.bind(this)}
+        />
+      );
     }
   }
 
   render() {
     return (
-      <div className="track-controls">
-        <div className="track-control-buttons">
+      <div>
+        <div style={styles.controlButtonsContainer}>
           <img
-            className="previous"
             src="/img/previous.svg"
-            onClick={this.playPrevious.bind(this)} />
+            alt="Previous"
+            style={styles.controlButtons}
+            onClick={this.playPrevious.bind(this)}
+          />
           {this.playState()}
-          <img src="/img/next.svg" className="next" />
-        </div>
-        <div className="volume-control">
           <img
-            className="volume-icon"
+            src="/img/next.svg"
+            alt="Next"
+            style={styles.controlButtons}
+            onClick={this.playNext.bind(this)}
+          />
+        </div>
+        <div style={styles.volumeControlContainer}>
+          <img
             src="/img/volume.svg"
-            onClick={this.playNext.bind(this)} />
+            alt="Volume"
+            style={styles.volumeIcon}
+          />
           <input
             className="volume"
+            style={styles.input}
             type="range"
             value={this.state.volume}
-            onChange={event => this.setState({volume: event.target.value})}
-            onMouseUp={event => this.setVolume(event.target.value)} />
+            onChange={event => this.setState({ volume: event.target.value })}
+            onMouseUp={event => this.setVolume(event.target.value)}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
