7e26d6fa025ad5fa9b4b5694c7740f05a3588b30
Bobby King <king.robertjoseph@gmail.com>
2016-10-10 23:29:54 -0400

    Change component for structure of line display

--- src/client/app/components/MTACurrentLine.jsx
+++ src/client/app/components/MTACurrentLine.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import styles from './MTACurrentLine_style.js';
 
 const MTACurrentLine = (props) => {
+  const subwayLines = props.currentLine.split('').map((line, i) => {
+    styles.subwayLine.backgroundColor = styles[`line${props.currentLine}`].backgroundColor;
+    return (
+      <div style={styles.subwayLine} key={i}>
+        {line}
+      </div>
+    );
+  });
+
   return (
     <div style={styles.div}>
-      <h2 style={styles.h2}>{ props.currentLine }</h2>
+      <div style={styles.itemContainer}>
+        {subwayLines}
+      </div>
       <h3>{ props.currentLineStatus }</h3>
       <p style={styles.p}>{ props.currentLineText }</p>
     </div>
--- src/client/app/components/MTACurrentLine_style.js
+++ src/client/app/components/MTACurrentLine_style.js
@@ -3,8 +3,56 @@ const styles = {
     width: '66%',
     paddingLeft: '40px',
   },
-  h2: {
-    margin: '0',
+  itemContainer: {
+    display: 'flex',
+    flexFlow: 'row',
+    justifyContent: 'center',
+    margin: '5px 0',
+    padding: '10px 0',
+  },
+  subwayLine: {
+    backgroundColor: '#222',
+    borderRadius: '50%',
+    boxShadow: '0 3px 3px #222',
+    fontSize: '35px',
+    height: '50px',
+    lineHeight: '50px',
+    margin: '0 5px',
+    textShadow: '0 1px 1px #222',
+    width: '50px',
+  },
+  lineACE: {
+    backgroundColor: '#0039A6',
+  },
+  lineBDFM: {
+    backgroundColor: '#FF6319',
+  },
+  lineG: {
+    backgroundColor: '#6CBE45',
+  },
+  lineJZ: {
+    backgroundColor: '#996633',
+  },
+  lineL: {
+    backgroundColor: '#A7A9AC',
+  },
+  lineNQR: {
+    backgroundColor: '#FCCC0A',
+  },
+  line123: {
+    backgroundColor: '#EE352E',
+  },
+  line456: {
+    backgroundColor: '#00933C',
+  },
+  line7: {
+    backgroundColor: '#B933AD',
+  },
+  lineS: {
+    backgroundColor: '#808183',
+  },
+  lineSIR: {
+    backgroundColor: '#21308E',
   },
   p: {
     textAlign: 'left',
