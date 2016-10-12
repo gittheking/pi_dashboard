ee6922a872689a4da8a6bf9fe61796b51f88e0f3
Bobby King <king.robertjoseph@gmail.com>
2016-10-12 12:04:54 -0400

    Convert Sonos component's styling to css-modules

---
+++ src/client/app/components/Sonos/Sonos.jsx
@@ -0,0 +1,53 @@
+import React            from 'react';
+import MusicInfo        from './MusicInfo/MusicInfo.jsx';
+import MusicControls    from './MusicControls/MusicControls.jsx';
+import styles           from './Sonos.css';
+
+export default class Sonos extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      artist: 'Artist',
+      track: 'Track',
+      album: 'Album',
+      albumCoverURL: '',
+    };
+
+    this.getTrackInfo();
+  }
+
+  getTrackInfo() {
+    fetch('/music/track')
+    .then(response => response.json())
+    .then((result) => {
+      this.setState({
+        artist: result.trackInfo.artist,
+        track: result.trackInfo.title,
+        album: result.trackInfo.album,
+        albumCoverURL: result.trackInfo.albumArtURL,
+        volume: '50',
+      });
+    })
+    .catch(err => console.log('Fetch error: ', err));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 className={styles.heading}>SONOS</h1>
+        <MusicInfo
+          artist={this.state.artist}
+          track={this.state.track}
+          album={this.state.album}
+          albumCoverURL={this.state.albumCoverURL}
+        />
+        <MusicControls
+          playState={this.state.playState}
+          volume={this.state.volume}
+        />
+      </div>
+    );
+  }
+}
