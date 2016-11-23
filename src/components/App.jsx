d865e06e03dea03bb408fe6fe423671b63ae2e1b
Bobby King <king.robertjoseph@gmail.com>
2016-11-22 20:54:31 -0500

    Make App.jsx default to clock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
-import Sonos from './Sonos/Sonos.jsx';
-import MTA from './MTA/MTA.jsx';
 import mtaLogo from '../img/MTA_NYC_logo.svg';
 import clockLogo from '../img/clock-logo.svg';
 import musicLogo from '../img/music-logo.svg';
 import weatherLogo from '../img/weather-logo.svg';
+import NavBar from './NavBar.jsx';
 import style from './App.css';
+import moment from 'moment';
 
-export default class App extends React.Component {
+export default class App extends Component {
 
   constructor() {
     super();
@@ -18,41 +17,26 @@ export default class App extends React.Component {
     };
   }
 
+  handleAppRender() {
+    if (this.props.children) {
+      return this.props.children;
+    } else {
+      return moment().format('LT');
+    }
+  }
+
   handleAppToggle(e) {
-    console.log(e.target.id);
-    if(e.target.id.indexOf('clock') >= 0) this.setState({ activeApp: 'clock' });
-    if(e.target.id.indexOf('mta') >= 0) this.setState({ activeApp: 'mta' });
-    if(e.target.id.indexOf('music') >= 0) this.setState({ activeApp: 'music' });
-    if(e.target.id.indexOf('weather') >= 0) this.setState({ activeApp: 'weather' });
-    console.log(this.state);
+    // if(e.target.id.indexOf('clock') >= 0) this.setState({ activeApp: 'clock' });
+    // if(e.target.id.indexOf('mta') >= 0) this.setState({ activeApp: 'mta' });
+    // if(e.target.id.indexOf('music') >= 0) this.setState({ activeApp: 'music' });
+    // if(e.target.id.indexOf('weather') >= 0) this.setState({ activeApp: 'weather' });
   }
 
   render() {
     return (
       <div>
-        <div id={style['app-nav']}>
-          <Link to={'/'} onClick={event => this.handleAppToggle(event)}>
-            <div id="clock-nav" className={style['nav-logos']}>
-              <img id={style['clock-logo']} src={clockLogo} alt="Clock"/>
-            </div>
-          </Link>
-          <Link to={'/mta'} onClick={event => this.handleAppToggle(event)}>
-            <div id="mta-nav" className={style['nav-logos']}>
-              <img id={style['mta-logo']} src={mtaLogo} alt="MTA"/>
-            </div>
-          </Link>
-          <Link to={'/sonos'} onClick={event => this.handleAppToggle(event)}>
-            <div id="sonos-nav" className={style['nav-logos']}>
-              <img id={style['music-logo']} src={musicLogo} alt=""/>
-            </div>
-          </Link>
-          <Link to={'/'} onClick={event => this.handleAppToggle(event)}>
-            <div id="weather-nav" className={style['nav-logos']}>
-              <img id={style['weather-logo']} src={weatherLogo} alt=""/>
-            </div>
-          </Link>
-        </div>
-        {this.props.children}
+        <NavBar />
+        {this.handleAppRender()}
       </div>
     );
   }
