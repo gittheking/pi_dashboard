1079022e85ac9fab40b3f3910009ed20549d5da7
Bobby King <king.robertjoseph@gmail.com>
2016-11-26 13:53:54 -0500

    Add weather route to server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path    = require('path');
 const home    = require('./routes/home');
 const train   = require('./routes/train');
 const music   = require('./routes/music');
-
+const weather = require('./routes/weather');
 const app     = express();
 const port    = process.env.PORT || 3000;
 
@@ -18,3 +18,4 @@ app.listen(port, () => console.log('Server is listening on port: ',port));
 app.use('/', home);
 app.use('/train', train);
 app.use('/music', music);
+app.use('/weather', weather);
