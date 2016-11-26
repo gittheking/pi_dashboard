e1580a168a9ca065d911a592d3d2d46cbb58ebdb
Bobby King <king.robertjoseph@gmail.com>
2016-11-26 13:54:50 -0500

    Add GET routes for /current and /forecast

--- routes/weather.js
+++ routes/weather.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
+const { getFiveDayForecast,
+        getCurrentWeather } = require('../services/weather');
 
-router.get('/', (req, res) => {
-  res.json({});
+router.get('/current', getCurrentWeather, (req, res) => {
+  res.json({ weather: res.currentWeather });
+});
+
+router.get('/forecast', getFiveDayForecast, (req, res) => {
+  res.json({ forecast: res.forecast, days: res.days });
 });
 
 module.exports = router;
